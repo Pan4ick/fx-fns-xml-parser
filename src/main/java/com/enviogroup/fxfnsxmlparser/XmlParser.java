08bfb84e0dbfce62e20aacf59fac8ea14afe9154
Alexander <grimasa904@gmail.com>
2022-12-20 04:21:29 +0300

    Фикс бага, при котором не создавался тэг, которого не существует

--- src/main/java/com/enviogroup/fxfnsxmlparser/XmlParser.java
+++ src/main/java/com/enviogroup/fxfnsxmlparser/XmlParser.java
@@ -8,6 +8,7 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.transform.*;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpressionException;
@@ -91,7 +92,8 @@ public class XmlParser implements Tags {
     public void saveChanges(File file) throws XPathExpressionException, TransformerException {
         setCompanyInformation(GRUZ_OTPR_ID_PATH, orgNameSender, innSender, kppSender, GRUZ_OTPR_ADDRESS_PATH, countryCodeSender, addressSender);
         setCompanyInformation(GRUZ_SV_YUL_UCH_PATH, orgNameConsignee, innConsignee, kppConsignee, GRUZ_ADDRESS_PATH, countryCodeConsignee, addressConsignee);
-        Element elementCargo = setElementByPath(TRAN_GRUZ_PATH, xPath, document);
+        Element elementCargo = setOrCreateElement(TRAN_GRUZ_PATH);
+//                setElementByPath(TRAN_GRUZ_PATH, xPath, document)
         if (!cargo.isEmpty()) {
             elementCargo.setAttribute(SV_TRAN_GRUZ, cargo);
         }
@@ -103,7 +105,15 @@ public class XmlParser implements Tags {
                 elementList.remove(elementList.size() - 1);
             } else {
                 Element newElementOsnPer = document.createElement(OSN_PER);
-                elementSvPer.insertBefore(newElementOsnPer, elementSvPer.getElementsByTagName(SV_LIC_PER).item(0));
+                elementSvPer.appendChild(document.createTextNode("\n"));
+                if (setElementByPath(SV_LIC_PER_PATH, xPath, document) != null) {
+                    elementSvPer.insertBefore(newElementOsnPer, elementSvPer.getElementsByTagName(getLastTag(SV_LIC_PER_PATH)).item(0));
+                } else if (setElementByPath(TRAN_GRUZ_PATH, xPath, document) != null) {
+                    elementSvPer.insertBefore(newElementOsnPer, elementSvPer.getElementsByTagName(getLastTag(TRAN_GRUZ_PATH)).item(0));
+                } else {
+                    elementSvPer.appendChild(newElementOsnPer);
+                }
+                elementSvPer.appendChild(document.createTextNode("\n"));
                 elementSvPer.normalize();
                 elementList.add(newElementOsnPer);
             }
@@ -113,30 +123,63 @@ public class XmlParser implements Tags {
             elementList.get(i).setAttribute(NOM_OSN, agreements.get(i).getDocumentNumber());
             elementList.get(i).setAttribute(DATE_OSN, agreements.get(i).getDocumentDate());
         }
-        Transformer transformer = TransformerFactory.newInstance().newTransformer();
-        transformer.setOutputProperty(OutputKeys.INDENT, "no");
+        File style = new File("src\\main\\resources\\stylesheet.xml");
+        StreamSource streamSource = new StreamSource(style);
+        Transformer transformer = TransformerFactory.newInstance().newTransformer(streamSource);
+        //transformer.setOutputProperty(OutputKeys.INDENT, "yes");
         Result output = new StreamResult(file);
         Source input = new DOMSource(document);
         transformer.transform(input, output);
     }
 
+    private Element setOrCreateElement(String path) throws XPathExpressionException {
+        try {
+            Element element;
+            if (setElementByPath(path, xPath, document) != null) {
+                element = setElementByPath(path, xPath, document);
+            } else {
+                element = document.createElement(getLastTag(path));
+                Element parentElement = setElementByPath(getParentPath(path), xPath, document);
+                parentElement.appendChild(document.createTextNode("\n"));
+                parentElement.appendChild(element);
+                parentElement.appendChild(document.createTextNode("\n"));
+                parentElement.normalize();
+            }
+            return element;
+        } catch (Exception exception) {
+            return null;
+        }
+    }
+
+    private String getLastTag(String tagPath) {
+        return tagPath.substring(tagPath.lastIndexOf("/") + 1);
+    }
+
+    private String getParentPath(String tagPath) {
+        return tagPath.substring(0, tagPath.lastIndexOf("/"));
+    }
+
     private void setCompanyInformation(String infoPath, String orgName, String inn, String kpp, String addressPath, String countryCode, String address) throws XPathExpressionException {
-        Element element = setElementByPath(infoPath, xPath, document);
-        if (!orgName.isEmpty()) {
-            element.setAttribute(NAIM_ORG, orgName);
+        Element element = setOrCreateElement(infoPath);
+        if (element != null) {
+            if (!orgName.isEmpty()) {
+                element.setAttribute(NAIM_ORG, orgName);
+            }
+            if (!inn.isEmpty()) {
+                element.setAttribute(INN, inn);
+            }
+            if (!kpp.isEmpty()) {
+                element.setAttribute(KPP, kpp);
+            }
         }
-        if (!inn.isEmpty()) {
-            element.setAttribute(INN, inn);
-        }
-        if (!kpp.isEmpty()) {
-            element.setAttribute(KPP, kpp);
-        }
-        Element elementAddress = setElementByPath(addressPath, xPath, document);
-        if (!countryCode.isEmpty()) {
-            elementAddress.setAttribute(COD_STR, countryCode);
-        }
-        if (!address.isEmpty()) {
-            elementAddress.setAttribute(ADR_TEXT, address);
+        Element elementAddress = setOrCreateElement(addressPath);
+        if (elementAddress != null) {
+            if (!countryCode.isEmpty()) {
+                elementAddress.setAttribute(COD_STR, countryCode);
+            }
+            if (!address.isEmpty()) {
+                elementAddress.setAttribute(ADR_TEXT, address);
+            }
         }
     }
 
